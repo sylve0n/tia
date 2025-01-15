210fcc680897ce4f4028beb81a8445fd41f0f10e
Hannah Stewart <hannahsartin@gmail.com>
2025-01-15 13:48:02 -0600

    testing component

--- components/content/Test.vue
+++ components/content/Test.vue
@@ -2,8 +2,18 @@
 </script>
 
 <template>
-  <ContentSlot name="item"/>
-  <div>
+  <h2><ContentSlot name="item"/></h2>
+  <div class="whatever">
     <ContentSlot name="whatever"/>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  h2 {
+    color: yellowgreen;
+  }
+  .whatever {
+    color: white;
+    background-color: indigo;
+  }
+</style>
